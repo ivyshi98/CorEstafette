f3b23b9d4015295b323da081fb6ef2baf68ee494
Ivy Shi <jia.chen.shi@duke.edu>
2021-04-30 11:40:31 -0400

    Modified setters and getters in message and response

--- WebClient/src/IMessage.ts
+++ WebClient/src/IMessage.ts
@@ -1,13 +1,9 @@
 ﻿export interface IMessage {
-    //correlationId: string;
-    //content: string;
-    //sender: string;
-    //topic: string;
-
     Topic : string;
     Content : string;
     Sender : string ;
     CorrelationId : string;
+    TimeStamp : Date;
 }
 
   
--- WebClient/src/Message.ts
+++ WebClient/src/Message.ts
@@ -5,27 +5,28 @@ export class Message implements IMessage {
     private content: string;
     private sender: string;
     private topic: string;
+    private timeStamp: Date;
 
     constructor(id: string, content: string, sender: string, topic: string) {
         this.correlationId = id;
         this.content = content;
         this.sender = sender;
         this.topic = topic;
+        this.timeStamp = new Date();
     }
 
-    get Topic() {
-        return this.topic;
-    }
+    get Topic() { return this.topic; }
+    set Topic(value: string) { this.topic = value;}
 
-    get CorrelationId() {
-        return this.correlationId;
-    }
+    get CorrelationId() { return this.correlationId; }
+    set CorrelationId(value: string) { this.correlationId = value; }
 
-    get Sender() {
-        return this.sender;
-    }
+    get Sender() { return this.sender; }
+    set Sender(value: string) { this.sender = value; }
 
-    get Content() {
-        return this.content;
-    }
+    get Content() { return this.content; }
+    set Content(value: string) { this.content = value; }
+
+    get TimeStamp() { return this.timeStamp; }
+    set TimeStamp(value: Date) { this.timeStamp = value; }
 }
